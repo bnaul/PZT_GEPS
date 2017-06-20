e3f0915b2d004677494399a32cebb5dec7196206
Brett Naul <brettnaul@gmail.com>
2017-06-20 13:16:16 -0700

    Add convolutional autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -4,8 +4,8 @@ from scipy import signal
 import h5py
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import (Dense, Conv1D, GRU, LSTM, Recurrent, Bidirectional,
-                          TimeDistributed, Dropout, Flatten, RepeatVector, Reshape)
+from keras.layers import (Dense, Conv1D, GRU, LSTM, Recurrent, Bidirectional, TimeDistributed,
+                          Dropout, Flatten, RepeatVector, Reshape, MaxPooling1D, UpSampling1D)
 
 
 def load_data(data_path, resample_size=256):
@@ -16,16 +16,19 @@ def load_data(data_path, resample_size=256):
     for i in range(X.shape[0]):
         X_resample[i] = signal.resample(X[i], 40 * resample_size)
     X_resample = X_resample.reshape((-1, resample_size))
-    X_resample -= np.mean(X_resample)
+    X_resample -= np.mean(X_resample)  # TODO remove mean of each singal?
     X_resample /= np.std(X_resample)
     X_resample = np.atleast_3d(X_resample)
 
     return X_resample
 
 
-def get_run_id(layer_type, size, num_layers, embedding, lr, drop_frac, batch_size, **kwargs):
-    return (f"{layer_type}{size:03d}_x{num_layers}_emb{embedding:03d}_{lr:1.0e}"
-            f"_drop{int(100 * drop_frac)}_batch{batch_size}").replace('e-', 'm')
+def get_run_id(layer_type, size, num_layers, embedding, lr, drop_frac, batch_size, kernel_size, **kwargs):
+    run = (f"{layer_type}{size:03d}_x{num_layers}_emb{embedding:03d}_{lr:1.0e}"
+           f"_drop{int(100 * drop_frac)}_batch{batch_size}").replace('e-', 'm')
+    if layer_type == 'conv':
+        run += f'_k{kernel_size}'
+    return run
 
 
 def rnn_auto(layer, size, num_layers, embedding, n_step, drop_frac=0., bidirectional=True,
@@ -38,16 +41,32 @@ def rnn_auto(layer, size, num_layers, embedding, n_step, drop_frac=0., bidirecti
     model.add(wrapper(layer(size, return_sequences=(num_layers > 1)),
                         input_shape=(n_step, 1)))
     for i in range(1, num_layers):
-        model.add(wrapper(layer(size, return_sequences=(i < num_layers - 1))))
-        if drop_frac > 0.:
-            model.add(Dropout(drop_frac))
+        model.add(wrapper(layer(size, return_sequences=(i < num_layers - 1), dropout=drop_frac)))
     model.add(Dense(embedding, activation='linear', name='encoding'))
     model.add(RepeatVector(n_step))
     for i in range(num_layers):
-        model.add(wrapper(layer(size, return_sequences=True)))
-        if drop_frac > 0.:
-            model.add(Dropout(drop_frac))
+        model.add(wrapper(layer(size, return_sequences=True, dropout=drop_frac)))
     model.add(TimeDistributed(Dense(1, activation='linear')))
+
+    return model
+
+
+def conv_auto(size, num_layers, embedding, n_step, kernel_size, drop_frac=0., **kwargs):
+    """TODO: batch norm?"""
+    model = Sequential()
+    model.add(Conv1D(size, kernel_size, padding='same', activation='relu',
+                     input_shape=(n_step, 1)))
+    for i in range(1, num_layers):
+        model.add(MaxPooling1D(2))
+        model.add(Conv1D(size, kernel_size, padding='same', activation='relu'))
+
+    model.add(Dense(embedding, activation='linear', name='encoding'))
+#    model.add(Conv1D(embedding, kernel_size=1, activation='linear', name='encoding'))
+
+    for i in range(1, num_layers):
+        model.add(Conv1D(size, kernel_size, padding='same', activation='relu'))
+        model.add(UpSampling1D(2))
+    model.add(Conv1D(size, kernel_size, padding='same', activation='relu'))
 
     return model
 
@@ -71,6 +90,7 @@ def main(arg_dict=None):
     parser.add_argument("--lr", type=float)
     parser.add_argument("--layer_type", type=str)
     parser.add_argument("--N_train", type=int)
+    parser.add_argument("--kernel_size", type=int, default=5)
 #    parser.add_argument("--patience", type=int, default=20)
     parser.add_argument('--bidirectional', dest='bidirectional', action='store_true')
     parser.add_argument('--overwrite', dest='overwrite', action='store_true')
@@ -80,10 +100,10 @@ def main(arg_dict=None):
     if arg_dict:  # merge additional arguments w/ defaults
         args = Namespace(**{**args.__dict__, **arg_dict})
 
-#    if args.layer_type == 'conv' and args.filter_length is None:
-#        parser.error("--layer_type {} requires --filter_length".format(args.layer_type))
-
-    X = load_data(args.data_path, args.n_cycles)
+#    X = load_data(args.data_path, args.n_cycles)
+    X = np.load('data/X_256.npy')
+    if args.n_cycles != 256:
+        raise ValueError("256 hard coded")
     if args.N_train:
         train = np.arange(args.N_train)
     else:
@@ -104,8 +124,13 @@ def main(arg_dict=None):
     if issubclass(layer, Recurrent):
         model = rnn_auto(layer, args.size, args.num_layers, args.embedding, n_step=X.shape[1],
                          drop_frac=args.drop_frac)
+
+    if issubclass(layer, Conv1D):
+#def conv_auto(size, num_layers, embedding, n_step, kernel_size, drop_frac=0., **kwargs):
+        model = conv_auto(args.size, args.num_layers, args.embedding, n_step=X.shape[1],
+                          kernel_size=args.kernel_size, drop_frac=args.drop_frac)
     else:
-        raise NotImplementedError("TODO convolutional")
+        raise NotImplementedError()
     model.compile(Adam(args.lr), loss='mse')
 
     history = model.fit(X[train], X[train], epochs=args.epochs, batch_size=args.batch_size,
